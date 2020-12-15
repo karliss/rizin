91dfa1bb0c209dc2cd909d798327c3b91ef7f74e
Riccardo Schirone <sirmy15@gmail.com>
2020-12-15 15:07:16 +0100

    Revert "Revert "Use O_CLOEXEC when creating pipes so they are closed on exec""
    This reverts commit b826f30cbe512a0a1be7f1dff696df1fcc302414.

--- test/unit/test_subprocess.c
+++ test/unit/test_subprocess.c
@@ -130,6 +130,7 @@ bool test_specialchar_args(void) {
 }
 
 bool all_tests () {
+	// TODO: use rz_subprocess in cmd_system (!)
 	mu_run_test (test_noargs_noinput_outerr);
 	mu_run_test (test_args);
 	mu_run_test (test_env);
